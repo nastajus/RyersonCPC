20e2afb0925e64a58fdf729de4e2dea08af932ff
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-25 10:49:06 -0500

    Added 2 solutions of mine  (Open Kattis level 1)

--- src/Aaah.java
+++ src/Aaah.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
 /**
- * Created by Dante on 2014-10-09.
+ * Created by IAN on 24/11/14.
  */
 public class Aaah {
-    public static void main(String[] Args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String jon = sc.nextLine();
-        String doc = sc.nextLine();
+        String line1 = sc.nextLine();
+        String line2 = sc.nextLine();
+        if ( line2.length() <= line1.length() ) System.out.println("go");
+        else System.out.println("no");
+        //System.out.println(sc.nextLine());
 
-        if (doc.length()>jon.length()) System.out.println("no");
-        else System.out.println("go");
+
     }
 }
--- src/Carrots.java
+++ src/Carrots.java
@@ -1,1 +1,12 @@
-import java.util.Scanner; public class Carrots {public static void main(String[] Args) {Scanner sc = new Scanner(System.in);sc.nextInt();System.out.println(sc.nextInt());}}
+import java.util.Scanner;
+
+/**
+ * Created by IAN on 24/11/14.
+ */
+public class Carrots {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        sc.nextInt();
+        System.out.println(sc.nextInt());
+    }
+}
