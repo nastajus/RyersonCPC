fb556d85ba36b7738a4db5c4ce8860d3eace5681
Ian Nastajus <ian.nastajus@gmail.com>
2016-10-09 00:58:43 -0400

    refactored VirtualFriends to include a list of maps, obeying the last sentence more carefully
    despite the absence of sample output data, this does pass my local testing of this expanded test scenario. I must still be missing some edgecase uknown to me

--- src/VirtualFriends.java
+++ src/VirtualFriends.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by IAN on 09/10/2016.
@@ -8,7 +6,7 @@ import java.util.Scanner;
 public class VirtualFriends {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        Map<String, Boolean> names = new HashMap<>();
+        List<Map<String,Boolean>> networks = new ArrayList<>();
 
         String str = sc.nextLine();
         int numTestCases = Integer.parseInt(str);
@@ -23,11 +21,32 @@ public class VirtualFriends {
                 String regex = "\\s";    //space character, escaped once for regex, second for java
                 String[] parts = str.split(regex);
 
-                for (String part : parts){
-                    names.put(part, true);
+                boolean personsExistsAlready = false;
+
+                //check existing networks for any matches
+                for (Map<String, Boolean> network : networks){
+
+                    //name exists in pre-existing network
+                    if (network.containsKey(parts[0]) || network.containsKey(parts[1])) {
+
+                        //at this point it doesn't matter, set both to true in said network
+                        network.put(parts[0], true);
+                        network.put(parts[1], true);
+                        personsExistsAlready = true;
+                        System.out.println(network.size());
+
+                        break;
+                    }
                 }
 
-                System.out.println(names.size());
+                if (!personsExistsAlready){
+                    Map<String, Boolean> friendships = new HashMap<>();
+                    friendships.put(parts[0], true);
+                    friendships.put(parts[1], true);
+                    networks.add(friendships);
+                    System.out.println(friendships.size());
+
+                }
             }
         }
     }
