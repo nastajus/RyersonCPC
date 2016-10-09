1041a4644a5c5829c331efea5a59c5f093a5c11e
Ian Nastajus <ian.nastajus@gmail.com>
2016-10-09 00:38:23 -0400

    removed regex isNumeric, added explicit ranges to expect now.... still failing on test 3/5

--- src/VirtualFriends.java
+++ src/VirtualFriends.java
@@ -10,31 +10,25 @@ public class VirtualFriends {
         Scanner sc = new Scanner(System.in);
         Map<String, Boolean> names = new HashMap<>();
 
-        while(sc.hasNextLine()){
-            String str = sc.nextLine();
+        String str = sc.nextLine();
+        int numTestCases = Integer.parseInt(str);
 
-            //blatantly disregard counting per test case, only requires to reset when new numbers detected
-            //assumes only valid data provided
-            if (isNumeric(str)) {
-                names = new HashMap<>();
-                continue;
+        for (int tc = 0; tc < numTestCases; tc++){
+            str = sc.nextLine();
+            int numFriendshipsFormed = Integer.parseInt(str);
+
+            for(int f = 0; f < numFriendshipsFormed; f++){
+                str = sc.nextLine();
+
+                String regex = "\\s";    //space character, escaped once for regex, second for java
+                String[] parts = str.split(regex);
+
+                for (String part : parts){
+                    names.put(part, true);
+                }
+
+                System.out.println(names.size());
             }
-
-            String regex = "\\s";    //space character, escaped once for regex, second for java
-            String[] parts = str.split(regex);
-
-            for (String part : parts){
-                names.put(part, true);
-            }
-
-            System.out.println(names.size());
-
         }
     }
-
-    public static boolean isNumeric(String str)
-    {
-        return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
-    }
-
 }
